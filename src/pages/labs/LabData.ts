d80056642f056b11055b66daca62d611c801af50
TheRealLoppiX <tutuzao2016@gmail.com>
2025-09-12 17:06:20 -0300

    2.0.8

--- src/pages/labs/LabData.ts
+++ src/pages/labs/LabData.ts
@@ -14,20 +14,20 @@ export const sqlInjectionLabs: Lab[] = [
     title: 'SQL Injection - Recuperar Dados Ocultos',
     description: 'Use uma vulnerabilidade de SQL Injection para contornar um filtro e exibir todos os produtos da base de dados.',
     path: '/labs/sql-injection/1',
-    iframeSrc: '/labs/lab-sqli-1.html'
+    iframeSrc: '/labs/SQLi Lab 1 - Dados Ocultos.html'
   },
   { 
     id: '2', 
     title: 'SQL Injection - Contornar Autenticação',
     description: 'Explore uma falha de SQL Injection num formulário de login para aceder como o utilizador administrador.',
     path: '/labs/sql-injection/2',
-    iframeSrc: '/labs/lab-sqli-2.html'
+    iframeSrc: '/labs/SQLi Lab 2 - Contornar Login.html'
   },
   { 
     id: '3', 
     title: 'SQL Injection - Ataque UNION',
     description: 'Utilize um ataque UNION para extrair dados de uma "tabela" diferente e exibi-los na página.',
     path: '/labs/sql-injection/3',
-    iframeSrc: '/labs/lab-sqli-3.html'
+    iframeSrc: '/labs/SQLi Lab 3 - Ataque UNION.html'
   },
 ];
